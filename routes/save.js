b6fd2bb6d1a2f8c6dd751b8a518ebc1248303501
Adrian Mostowski <adaxelx@o2.pl>
2020-09-28 18:22:52 +0200

    fix paths

--- routes/save.js
+++ routes/save.js
@@ -1,54 +1,61 @@
 var express = require("express");
 var router = express.Router();
 const fs = require("fs");
+const path = require("path");
 
 router.post("/save", (req, res) => {
   const { gameData } = req.body;
   const date = new Date();
-  fs.writeFile(
-    `${__dirname}/files/catan-${date.toLocaleDateString("pl-PL", {
+  const dirName = path.join(
+    __dirname,
+    `/../files/catan-${date.toLocaleDateString("pl-PL", {
       hour: "2-digit",
       minute: "2-digit",
-    })}.json`,
-    JSON.stringify(gameData),
-    (err) => {
-      if (err) {
-        res.status(500);
-        res.json({ ...err, message: "coś się zjebało" });
-      }
-      res.status(200);
-      res.json({
-        message: `Poprawnie zapisano plik catan-${date.toLocaleDateString(
-          "pl-PL",
-          {
-            hour: "2-digit",
-            minute: "2-digit",
-          }
-        )}`,
-      });
-    }
+    })}.json`
   );
-});
-
-router.get("/file_names", (req, res) => {
-  fs.readdir(`${__dirname}/files`, (err, files) => {
+  fs.writeFile(dirName, JSON.stringify(gameData), (err) => {
     if (err) {
       res.status(500);
       res.json({ ...err, message: "coś się zjebało" });
     }
-    const arrayOfFiles = [];
-    console.log(files);
-    files?.forEach((file) => {
-      arrayOfFiles.push(file);
-    });
     res.status(200);
-    res.json(arrayOfFiles);
+    res.json({
+      message: `Poprawnie zapisano plik catan-${date.toLocaleDateString(
+        "pl-PL",
+        {
+          hour: "2-digit",
+          minute: "2-digit",
+        }
+      )}`,
+    });
+  });
+});
+
+router.get("/file_names", (req, res) => {
+  // res.json(`${__dirname}/files`);
+  const dirName = path.join(__dirname, `/../files`);
+  fs.readdir(dirName, (err, files) => {
+    if (err) {
+      res.status(500);
+      res.json({ err });
+      throw new Error(err);
+      return;
+    }
+    const arrayOfFiles = [];
+    if (files) {
+      files.forEach((file) => {
+        arrayOfFiles.push(file);
+      });
+      res.status(200);
+      res.json(arrayOfFiles);
+    }
   });
 });
 
 router.get("/game_stats/:fileName", (req, res) => {
   const { fileName } = req.params;
-  fs.readFile(`${__dirname}/files/${fileName}`, "utf8", (err, data) => {
+  const dirName = path.join(__dirname, `/../files/${fileName}`);
+  fs.readFile(dirName, "utf8", (err, data) => {
     if (err) {
       res.status(500);
       res.json({ message: "błąd" });
