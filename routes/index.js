aa4a5797fec1b7196f9f9e85bc722daa919a8cf0
Adrian Mostowski <adaxelx@o2.pl>
2020-09-28 18:29:01 +0200

    create directory

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const save = require("./save");
+const fs = require("fs");
+const path = require("path");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  const dirName = path.join(__dirname, "../files");
+  fs.mkdir(dirName, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
   res.json({ message: "działa :)" });
 });
 
